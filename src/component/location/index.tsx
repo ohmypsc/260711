cd69b7d4dcdf8d24927c70c5725a8351cff27fc7
ohmypsc <greatkto@gmail.com>
2025-10-24 19:20:39 +0900

    Update index.tsx

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -5,54 +5,56 @@ import { LazyDiv } from "../lazyDiv"
 import { LOCATION, LOCATION_ADDRESS } from "../../const"
 
 export const Location = () => {
-  return (
-    <>
-      <LazyDiv className="card location">
-        <h2 className="english">Location</h2>
-        <div className="addr">
-          {LOCATION}
-          <div className="detail">{LOCATION_ADDRESS}</div>
-        </div>
-        <Map />
-      </LazyDiv>
-      <LazyDiv className="card location">
-        <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <BusIcon className="transportation-icon" />
-          </div>
-          <div className="heading">대중교통</div>
-          <div />
-          <div className="content">
-            * 지하철 이용시
-            <br />
-             <b>갑천역 3번출구</b> 에서
-            <br />
-            → 돌다리 얘길 해 말아 고민고민
-          </div>
-          <div />
-        <div className="location-info">
-          <div className="transportation-icon-wrapper">
-            <CarIcon className="transportation-icon" />
-          </div>
-          <div className="heading">자가용</div>
-          <div />
-          <div className="content">
-            네이버 지도, 카카오 네비, 티맵 등 이용
-            <br />
-            <b>대전 유성컨벤션 웨딩홀</b> 검색
-            <br />
-            - 주차 요금은 무료입니다.
-            <br />
-     
-          </div>
-          <div />
-          <div className="content">
-            <b>
-             
-            </b>
-          </div>
-        </div>
-      </LazyDiv>
-    </>
-  )
+  return (
+    <>
+      <LazyDiv className="card location">
+        <h2 className="english">Location</h2>
+        <div className="addr">
+          {LOCATION}
+          <div className="detail">{LOCATION_ADDRESS}</div>
+        </div>
+        <Map />
+      </LazyDiv>
+      
+      <LazyDiv className="card location">
+        <div className="location-info"> {/* 대중교통 시작 */}
+          <div className="transportation-icon-wrapper">
+            <BusIcon className="transportation-icon" />
+          </div>
+          <div className="heading">대중교통</div>
+          <div />
+          <div className="content">
+            * 지하철 이용시
+            <br />
+             <b>갑천역 3번출구</b> 에서
+            <br />
+            → 돌다리 얘길 해 말아 고민고민
+          </div>
+          <div />
+        </div> {/* 대중교통 종료 */}
+        
+        <div className="location-info"> {/* 자가용 시작 */}
+          <div className="transportation-icon-wrapper">
+            <CarIcon className="transportation-icon" />
+          </div>
+          <div className="heading">자가용</div>
+          <div />
+          <div className="content">
+            네이버 지도, 카카오 네비, 티맵 등 이용
+            <br />
+            <b>대전 유성컨벤션 웨딩홀</b> 검색
+            <br />
+            - 주차 요금은 무료입니다.
+            <br />
+          </div>
+          <div />
+          <div className="content">
+            <b>
+             
+            </b>
+          </div>
+        </div> {/* 자가용 종료 */}
+      </LazyDiv>
+    </>
+  )
 }
