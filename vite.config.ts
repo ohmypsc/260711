c8bef0f31301aada9898471c67b842f9dac1ec23
ohmypsc <greatkto@gmail.com>
2025-11-19 19:03:56 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mysc/",
+  base: "./",  // ⭐ Pages + Actions 조합에서 가장 안정적
   plugins: [react()],
 });
