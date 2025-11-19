5fe3e9e4c983244f5117325ed9d74fb2adf14132
ohmypsc <greatkto@gmail.com>
2025-11-19 16:23:50 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";   // ⭐ 추가
 import App from "./App";
 import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    
+    <BrowserRouter basename="/mysc">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
