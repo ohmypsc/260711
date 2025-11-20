2154a15c9dade5cd2ab61c376f945e2214ea35e2
ohmypsc <greatkto@gmail.com>
2025-11-20 15:08:59 +0900

    Create Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import "./Button.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "outline" | "filled" | "ghost";
+  children: ReactNode;
+}
+
+export const Button = ({
+  variant = "outline",
+  children,
+  className = "",
+  ...rest
+}: ButtonProps) => {
+  return (
+    <button
+      {...rest}
+      className={`w-btn ${variant} ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
