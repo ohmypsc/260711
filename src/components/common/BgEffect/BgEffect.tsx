d960503b615e2ece4b2d7593b292d5b6a25cfe77
ohmypsc <greatkto@gmail.com>
2025-11-20 17:13:29 +0900

    Update BgEffect.tsx

--- src/components/common/BgEffect/BgEffect.tsx
+++ src/components/common/BgEffect/BgEffect.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react"
-import patelUrl from "../../icons/petal.png"
+import petalUrl from "@/image/petal.png";
 
 const X_SPEED = 0.6
 const X_SPEED_VARIANCE = 0.8
