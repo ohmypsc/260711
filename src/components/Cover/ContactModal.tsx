e2ff838c51ebf7f212acf020211280251cc898b1
ohmypsc <greatkto@gmail.com>
2025-11-20 16:28:15 +0900

    Update ContactModal.tsx

--- src/components/Cover/ContactModal.tsx
+++ src/components/Cover/ContactModal.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ContactInfoContext } from "@/ContactInfoProvider";
-import { ModalBase } from "@/components/Modal/ModalBase";
+import { ModalBase } from "@/components/common/Modal/ModalBase";
 import "./ContactModal.scss";
 
 export function ContactModal({ onClose }: { onClose: () => void }) {
