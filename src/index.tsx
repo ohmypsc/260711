f507c73670b7aa952580e635918ee352a16ab357
ohmypsc <greatkto@gmail.com>
2025-11-21 19:35:32 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { BgEffect } from "@/components/common/BgEffect/BgEffect";
 import { Cover } from "@/components/Cover/Cover";
-import { Invitaion } from "@/components/Invitation/Invitation";
+import { Invitation } from "@/components/Invitation/Invitation";
 import { Information } from "@/components/Information/Information";
 
 export default function MainWeddingPage() {
