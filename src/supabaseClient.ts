836e4f4bd1ca14b55bc1d8d737cc0c7caee2fd8d
ohmypsc <greatkto@gmail.com>
2025-11-24 14:29:36 +0900

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wwotrjuzpcbwlhhlyqxv.supabase.co";
-
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3b3RyanV6cGNid2xoaGx5cXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NDY3NzgsImV4cCI6MjA3NzIyMjc3OH0.mOCukDJioOQemoV9hiVLMuFYezF1nf3CsZyE5KlpIbo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
