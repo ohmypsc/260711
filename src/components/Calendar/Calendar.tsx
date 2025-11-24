a79c7c4a81527d75ae87f79c5d728f613215092c
ohmypsc <greatkto@gmail.com>
2025-11-24 13:18:16 +0900

    Update Calendar.tsx

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -105,7 +105,7 @@ export const Calendar = () => {
 
   return (
     <div className="calendar-container">
-      <h2 className="section-title">캘린더</h2>
+      <h2 className="section-title">예식 안내</h2>
 
       {/* ✅ 날짜/요일/시간 한 줄 */}
       <div className="calendar-topline">
