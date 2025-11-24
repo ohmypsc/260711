4c44e327b6eb340cfccd57e0ca5fcb28681c671c
ohmypsc <greatkto@gmail.com>
2025-11-24 09:34:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/260711-main/",
+  base: "/mysc/",
   plugins: [react()],
 
   resolve: {
