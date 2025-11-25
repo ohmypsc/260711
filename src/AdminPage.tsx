875e7c5b515089e55c51e742ee909131b43f5728
ohmypsc <greatkto@gmail.com>
2025-11-25 09:00:24 +0900

    Update AdminPage.tsx

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import "./AdminPage.scss";
 import { Button } from "@/components/common/Button/Button";
 import { Modal } from "@/components/common/Modal/Modal";
 import { supabase } from "@/supabaseClient";
