4fe23619140502556478ff0e4016fc4573041f9c
ohmypsc <greatkto@gmail.com>
2025-12-18 10:59:57 +0900

    Update Timeline.tsx

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -28,7 +28,7 @@ type Caption = {
 
 /** 타이틀 no-break */
 const captions: Caption[] = [
-  { imgIndex: 1, title: <span className="no-break">1989년에 태어난 승철이와대체뭐가문제야머리아파죽겠네짜증짜증제발좀</span> },
+  { imgIndex: 1, title: <span className="no-break">1989년에 태어난 승철이와대체뭐가문제야머리아파죽겠네짜증짜증제발좀전혀해결이 안되잖아 어쩌라는거야 챗지피티고 제미나이고 다 ㅄ들이네 휴 꺼져라</span> },
   { imgIndex: 2, title: <span className="no-break">1990년에 태어난 미영이가</span> },
   { imgIndex: 3, title: <span className="no-break">2024년 가을에 만나</span> },
   { imgIndex: 4, title: <span className="no-break">2024년 겨울,</span> },
