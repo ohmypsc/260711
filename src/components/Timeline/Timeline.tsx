c63d3cf193585894702cf924b0084b6da48e93ab
ohmypsc <greatkto@gmail.com>
2025-12-29 19:05:35 +0900

    Update Timeline.tsx

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -28,9 +28,9 @@ type Caption = {
 
 /** 타이틀 no-break */
 const captions: Caption[] = [
-  { imgIndex: 1, title: <span className="no-break">1989년에 태어난 승철이와</span> },
-  { imgIndex: 2, title: <span className="no-break">1990년에 태어난 미영이가</span> },
-  { imgIndex: 3, title: <span className="no-break">2024년 가을에 만나</span> },
+  { imgIndex: 1, title: <span className="no-break">1989년 가을,</span> },
+  { imgIndex: 2, title: <span className="no-break">1990년 봄,</span> },
+  { imgIndex: 3, title: <span className="no-break">2024년 가을,</span> },
   { imgIndex: 4, title: <span className="no-break">2024년 겨울,</span> },
   { imgIndex: 5, title: <span className="no-break">2025년 봄,</span> },
   { imgIndex: 6, title: <span className="no-break">2025년 여름,</span> },
